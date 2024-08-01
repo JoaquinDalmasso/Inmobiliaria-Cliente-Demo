f2ad37d248e9bdadf9e833871119e9564cd55718
JoaquinDalmasso <joacodalmasso@gmail.com>
2024-08-01 19:48:35 -0300

    first commit

---
+++ src/pages/VirtualTour.jsx
@@ -0,0 +1,10 @@
+
+export default function VirtualTour() {
+  return (
+    <iframe 
+    src="/VirtualTour/index.htm" 
+    style={{ width: '100%', height: '100vh', border: 'none' }}
+    title="Virtual Tour"
+  />
+  )
+}
